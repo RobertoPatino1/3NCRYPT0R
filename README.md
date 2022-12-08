ec828a01567d203e04e6076735214b94b3641096
RobertoPatino1 <rapatino@espol.edu.ec>
2022-12-08 01:38:03 -0500

    Modified README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 3NCRYPT0R
+## Directory logic:
+
+libs: contains the libraries for each encryption algorithm
+includes: contains the header files for both program functions and libraries
+implmt: Contanis the .c files with implementation for all the functions in the includes directory
---
+++ includes/functions/help.h
@@ -0,0 +1,11 @@
+#ifndef HELP_H
+#define HELP_H
+
+/*
+Function print_help prints a help menu for the user when the -h switch is called
+@param command: The command the user passes while using -h switch (in case there's any)
+@return void
+*/
+void print_help(char *command);
+
+#endif /* HELP_H */
